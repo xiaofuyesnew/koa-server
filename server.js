03d98d9a3ea9a0eea2484c488955e99d98ae4aed
xiaofuyesnew <xiaofuyesnew@qq.com>
2017-09-28 14:15:29 +0800

    new

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = new Koa()
 app.use(async (ctx, next) => {
     await next()
     ctx.response.type = 'text/html'
-    ctx.response.body = '<h1>开源说网站正在建设中...</h1>'
+    ctx.response.body = '<h1>开源说网站正在建设中....</h1>'
 })
 
 app.listen(3000)

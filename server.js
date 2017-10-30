97dcf1c30f914539ca2490f8b81f685d5d21943e
xiaofuyesnew <xiaofuyesnew@qq.com>
2017-10-30 12:31:17 +0800

    更新为用yarn构建依赖，并使用静态文件

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+const path = require('path')
 const Koa = require('koa')
+const KoaStatic = require('koa-static')
 const app = new Koa()
+const staticPath = './static'
 
+app.use(KoaStatic(
+    path.join(__dirname, staticPath)
+))
+/**
 app.use(async (ctx, next) => {
     await next()
+
     ctx.response.type = 'text/html'
     ctx.response.body = '<h1>开源说网站正在建设中....</h1>'
 })
-
+/**/
 app.listen(3000)
 
 console.log('app started at port 3000...')
